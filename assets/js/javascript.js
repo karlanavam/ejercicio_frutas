670ad2c185ca8097d5e42e7229f52fd701e78944
karlaNava <karla.nm8@gmail.com>
2017-03-20 11:10:16 -0600

    Commit inicial

---
+++ assets/js/javascript.js
@@ -0,0 +1,18 @@
+alert("holi");
+var arregloFrutas = [];
+var frutas = document.getElementById("frutasPorAgregar");
+
+
+
+function agregarLista() {
+arregloFrutas.push(frutas);
+return arregloFrutas;
+}
+
+console.log(arregloFrutas);
+
+/*
+  1. que el boton agrege el texto al arregloFrutas
+  2.
+*/
+console.log(agregarLista);
