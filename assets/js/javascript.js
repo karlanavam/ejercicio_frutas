2a46cfad6a4c7962af9c134231efcc9f7a7f3604
karlaNava <karla.nm8@gmail.com>
2017-03-20 12:04:24 -0600

    commit dos

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,18 +1,21 @@
-alert("holi");
 var arregloFrutas = [];
-var frutas = document.getElementById("frutasPorAgregar");
+var frutas;
+var frutasAgregadas;
+
+function agregarFrutas() {
+  frutas = document.getElementById("frutasIntroducidas").value;
+  frutasAgregadas = arregloFrutas.push(frutas);
+  return frutasAgregadas.value = boton;
+
+  console.log(frutas);
+  console.log(frutasAgregadas);
 
 
-
-function agregarLista() {
-arregloFrutas.push(frutas);
-return arregloFrutas;
 }
-
 console.log(arregloFrutas);
-
-/*
+console.log(agregarFrutas);
+  /*
   1. que el boton agrege el texto al arregloFrutas
-  2.
+  2. que el arreglo de frutas se llene según las frutas agregadas
+  3. Imprimir frutas agregadas a la lista
 */
-console.log(agregarLista);
